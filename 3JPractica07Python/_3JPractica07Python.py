f1351f55e363a58eb1c23c9db4f90078fed5b928
EduardoVP-0 <144479949+EduardoVP-0@users.noreply.github.com>
2023-10-19 20:17:13 -0600

    Agregar archivos de proyecto.

---
+++ 3JPractica07Python/_3JPractica07Python.py
@@ -0,0 +1,32 @@
+#EDUARDO VELAZQUEZ
+#PRACTICA 07
+capacidad=10
+elementos=[0]*capacidad
+tope=-1
+
+print("Teclea un elemento de la pila (Termina con -1)")
+
+x=0
+CLAVE=-1
+
+while x != CLAVE:
+    entrada = input()
+    if entrada.isdigit():
+       x = int(entrada)
+       if tope < capacidad-1:
+          tope += 1
+          elementos[tope] = x
+       else:
+            print("Excepcion: Pila llena")
+            break
+    else:
+        print("Excepcion: Entrada no valida")
+
+if tope >= 0:
+    print("Elementos de la Pila:",end=" ")
+    while tope >= 0:
+     x=elementos[tope]
+     tope-= 1
+     print(x,end=" ")
+else:
+    print("Pila Vacia")
